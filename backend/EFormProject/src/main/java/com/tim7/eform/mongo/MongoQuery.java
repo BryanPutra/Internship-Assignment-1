abbab46fd69df5d68a56f0d5f2c83dbcf897a50a
Michael Marcelino <55041641+MichaelM10@users.noreply.github.com>
2022-08-11 12:18:32 +0700

    Squashed commit of the following:
    commit 50908638e84367ce09d81d90c9f83badf55bc651
    Author: Michael Marcelino <55041641+MichaelM10@users.noreply.github.com>
    Date:   Wed Aug 10 16:05:21 2022 +0700
    
        Update MongoQuery.java
    
    commit 2738a23b1c25a424b4750a34c0f10be3dd10b98d
    Author: Michael Marcelino <55041641+MichaelM10@users.noreply.github.com>
    Date:   Wed Aug 10 15:22:44 2022 +0700
    
        Update MongoQuery.java
    
    commit 1696d713a2587f40510cb27ddbb386d44cbad14d
    Author: Michael Marcelino <55041641+MichaelM10@users.noreply.github.com>
    Date:   Wed Aug 10 13:49:47 2022 +0700
    
        Update MongoQuery.java

--- backend/EFormProject/src/main/java/com/tim7/eform/mongo/MongoQuery.java
+++ backend/EFormProject/src/main/java/com/tim7/eform/mongo/MongoQuery.java
@@ -2,41 +2,106 @@ package com.tim7.eform.mongo;
 
 import static com.mongodb.client.model.Filters.eq;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoOperations;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.stereotype.Controller;import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.mongodb.Mongo;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.tim7.eform.model.EnumRole;
+import com.tim7.eform.model.Role;
 import com.tim7.eform.model.User;
-import com.tim7.eform.repository.CustomUserRepository;
-import com.tim7.eform.repository.UserRepository;
+import com.tim7.eform.repository.RoleRepository;
+import com.tim7.eform.repository.UserRepository;;
 
 public class MongoQuery {
-	String uri = "mongodb://localhost:27017";
+	@Autowired
+	UserRepository repo;
+	@Autowired
+	PasswordEncoder encoder;
+	@Autowired
+	RoleRepository roleRepository;
 	
+	final String uri = "mongodb://localhost:27017";
+	private static String fieldName = "";
+	private static String filterVal = "";
 
-	public void test() {
-		String email = "bambangaja@gmail.com";
+	public Document getUser(String email, String cif, String ktpId) {
+		Document doc;
+		Map filter = filterSelection(email, cif, ktpId);
+		fieldName = (String) filter.get("filterName");
+		filterVal = (String) filter.get("value");
+		
 		try (MongoClient mongoClient = MongoClients.create(uri)) {
 	        MongoDatabase database = mongoClient.getDatabase("eform_project");
 	        MongoCollection<Document> collection = database.getCollection("users");
-	        Document doc = collection.find(eq("email", email)).first();
+	        doc = collection.find(eq(fieldName, filterVal)).first();
 	        System.out.println(doc.toJson());
 	    }
+		return doc;
+	}
+	
+	public Document getCollectedData(String email, String cif, String ktpId) {
+		Document returnDoc = new Document();
+		Document doc = getUser(email, cif, ktpId);
+		
+		returnDoc.append("collectedData", doc.get("collectedData"));
+		
+		return returnDoc;
+	}
+	
+	public void updateUser(String email, String cif, String ktpId, Document updatedDoc) {
+		Map filter = filterSelection(email, cif, ktpId);
+		fieldName = (String) filter.get("filterName");
+		filterVal = (String) filter.get("value");
+		
+		try (MongoClient mongoClient = MongoClients.create(uri)) {
+	        MongoDatabase database = mongoClient.getDatabase("eform_project");
+	        MongoCollection<Document> collection = database.getCollection("users");
+	        collection.updateOne(eq(fieldName,filterVal), updatedDoc);
+	    }
+	}
+	
+	public void insertNewUser(String email, String fullName, String password) {
+		Set<Role> roles = new HashSet<>();
+		
+		User newUser = new User(
+	    		fullName,
+	    		email,
+	    		encoder.encode(password)
+	    		);
+		
+		Role userRole = roleRepository.findByName(EnumRole.ROLE_USER)
+				.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+		roles.add(userRole);
+		newUser.setRoles(roles);
+		
+		repo.save(newUser);
+	}
+	
+	private Map filterSelection(String email, String cif, String ktpId) {
+		Map filterMap = new HashMap();
+		String filter;
+		
+		if(!email.isEmpty()) {
+			filter  = email;
+			filterMap.put("filterName", "email");
+		}else if(!cif.isEmpty()) {
+			filter = cif;
+			filterMap.put("filterName", "cif");
+		}else{
+			filter = ktpId;
+			filterMap.put("filterName", "ktpId");
+		}
+		filterMap.put("value",filter);
+		return filterMap;
 	}
 }
